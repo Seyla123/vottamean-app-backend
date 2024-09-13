8d2a1b2047ce88f7b06ec6b3ba82890a802513de
Seav Seyla <Mrrseyla.758@gmaill.com>
2024-09-13 13:49:04 +0700

    feature : add route get attendance

---
+++ routes/attendanceRoutes.js
@@ -0,0 +1,20 @@
+// Express library
+const express = require('express');
+
+// Controllers
+const attendanceController = require('../controllers/attendanceController');
+const authController = require('../controllers/authController');
+
+// Define Express Router
+const router = express.Router();
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+// Restrict all routes to admin only
+router.use(authController.restrictTo('admin'));
+
+// Info routes
+router.get('/', attendanceController.getAllAttendances);
+
+
+module.exports = router;
