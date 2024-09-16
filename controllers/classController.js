3c637489da779092fb23b72d4b06ca23fc754d03
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-16 19:01:55 +0700

    feature : create-class-api, add isBelongto function in class

--- controllers/classController.js
+++ controllers/classController.js
@@ -12,8 +12,9 @@ const {
   isValidDescription,
 } = require('../validators/validators');
 
-const isBelongsToAdmin = require('../utils/isBelongsToAdmin');
-// validate class record
+const { isBelongsToAdmin } = require('../utils/isBelongsToAdmin');
+
+// validate class record 
 const validateClassData = (req, res, next) => {
   const { class_name, description } = req.body;
   try {
@@ -28,8 +29,10 @@ const validateClassData = (req, res, next) => {
 // Create a new class
 exports.addClass = catchAsync(async (req, res, next) => {
   const school_admin_id = req.school_admin_id;
-  req.body.school_admin_id = school_admin_id;
-  validateClassData, factory.createOne(Class)(req, res, next);
+  validateClassData(req, res, async () => {
+    req.body.school_admin_id = school_admin_id;
+    await factory.createOne(Class)(req, res, next);
+  });
 });
 
 // Get all active classes
@@ -37,9 +40,9 @@ exports.addClass = catchAsync(async (req, res, next) => {
 exports.getAllClasses = catchAsync(async (req, res, next) => {
   factory.getAll(
     Class,
-    [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
-    { active: true , school_admin_id: req.school_admin_id }
-  )(req, res,next);
+    { school_admin_id: req.school_admin_id },
+    [{ model: SchoolAdmin, as: 'SchoolAdmin' }]
+  )(req, res, next);
 });
 
 // Get a single class
@@ -49,35 +52,23 @@ exports.getAllClasses = catchAsync(async (req, res, next) => {
 //   [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
 //   { active: true }
 // );
-exports.getClass = catchAsync(async(req , res , next) => {
+exports.getClass = catchAsync(async (req, res, next) => {
   factory.getOne(
     Class,
     'class_id',
     [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
-    {active: true, school_admin_id:req.school_admin_id}
-  )(req, res,next);
-})
-
-// Check for belongs
-// const checkIfBelongs = async (id, school_admin_id) => {
-//   const classRecord = await Class.findOne({
-//     where: {
-//       class_id: id,
-//       school_admin_id: school_admin_id
-//     }
-//   });
-//   if (!classRecord) {
-//     throw new AppError('No class record found or you do not have permission for this record', 404);
-//   }
-// }
+    { active: true, school_admin_id: req.school_admin_id }
+  )(req, res, next);
+});
 
 // Update a class
 exports.updateClass = catchAsync(async (req, res, next) => {
-  await isBelongsToAdmin(req.params.id, req.school_admin_id);
+  await isBelongsToAdmin(req.params.id, 'class_id', req.school_admin_id, Class);
   factory.updateOne(Class, 'class_id')(req, res, next);
 });
+
 // Delete the class
 exports.deleteClass = catchAsync(async (req, res, next) => {
-  await isBelongsToAdmin(req.params.id, req.school_admin_id);
+  await isBelongsToAdmin(req.params.id , 'class_id', req.school_admin_id , Class);
   factory.deleteOne(Class, 'class_id')(req, res, next);
 });
