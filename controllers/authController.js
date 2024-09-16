16e0677359fd783d5a1b9014eca23c8e213b29d8
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-16 18:35:07 +0700

    resolve : conflict in authController in class feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -103,17 +103,10 @@ exports.signup = catchAsync(async (req, res, next) => {
   );
 
   // 6. Construct the verification URL and send it via email.
-<<<<<<< HEAD
   // const verificationUrl = `${req.protocol}://${req.get(
   //   'host'
   // )}/api/v1/auth/verify-email/${verificationToken}?token=${tempToken}`;
   const verificationUrl = `http://localhost:5173/auth/verify-email/${verificationToken}?token=${tempToken}`;
-=======
-  const verificationUrl = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/auth/verifyEmail/${verificationToken}?token=${tempToken}`;
-  // const verificationUrl = `http://localhost:5173/auth/verify-email/${verificationToken}?token=${tempToken}`;
->>>>>>> 914ed6e (feature : create-class-api, fix classRoutes and functions)
 
   try {
     await sendVerificationEmail(email, verificationUrl);
