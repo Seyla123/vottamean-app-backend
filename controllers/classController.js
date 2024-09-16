911cebdacd39cf9b43c7f84f1600b6cbf3b4a09d
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-16 18:31:41 +0700

    Refactor : class controller

--- controllers/classController.js
+++ controllers/classController.js
@@ -6,77 +6,78 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
+// validators
+const {
+  isValidClassName,
+  isValidDescription,
+} = require('../validators/validators');
+
+const isBelongsToAdmin = require('../utils/isBelongsToAdmin');
+// validate class record
+const validateClassData = (req, res, next) => {
+  const { class_name, description } = req.body;
+  try {
+    isValidClassName(class_name);
+    isValidDescription(description);
+    next();
+  } catch (error) {
+    next(new AppError(error.message, 400));
+  }
+};
+
 // Create a new class
 exports.addClass = catchAsync(async (req, res, next) => {
-  const { class_name, description } = req.body;
-  const school_admin_id = req.params.admin_id; 
-  const newClass = await Class.create({
-    class_name,
-    description,
-    school_admin_id, 
-  });
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newClass,
-    },
-  });
+  const school_admin_id = req.school_admin_id;
+  req.body.school_admin_id = school_admin_id;
+  validateClassData, factory.createOne(Class)(req, res, next);
 });
-
-// Update a class
-exports.updateClass = factory.updateOne(Class, 'class_id');
 
 // Get all active classes
+// exports.getAllClasses = factory.getAll(Class, { active: true }, [{ model: SchoolAdmin, as: 'SchoolAdmin' }]);
 exports.getAllClasses = catchAsync(async (req, res, next) => {
-  const classes = await Class.findAll({
-    where: { active: true },
-    include: [{ model: SchoolAdmin, as: 'SchoolAdmin' }]
-  });
-  if (!classes || classes.length === 0) {
-    return next(new AppError('No active classes found', 404));
-  }
-  res.status(200).json({
-    status: 'success',
-    results: classes.length,
-    data: classes,
-  });
+  factory.getAll(
+    Class,
+    [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
+    { active: true , school_admin_id: req.school_admin_id }
+  )(req, res,next);
 });
-
 
 // Get a single class
-exports.getClass = catchAsync(async (req, res, next) => {
-  const classToFetch = await Class.findOne({
-    where: { 
-      class_id: req.params.id,
-      active: true 
-    },
-    include: [{ model: SchoolAdmin, as: 'SchoolAdmin' }]
-  });
+// exports.getClass = factory.getOne(
+//   Class,
+//   'class_id',
+//   [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
+//   { active: true }
+// );
+exports.getClass = catchAsync(async(req , res , next) => {
+  factory.getOne(
+    Class,
+    'class_id',
+    [{ model: SchoolAdmin, as: 'SchoolAdmin' }],
+    {active: true, school_admin_id:req.school_admin_id}
+  )(req, res,next);
+})
 
-  if (!classToFetch) {
-    return next(new AppError('No active class found with that ID', 404));
-  }
+// Check for belongs
+// const checkIfBelongs = async (id, school_admin_id) => {
+//   const classRecord = await Class.findOne({
+//     where: {
+//       class_id: id,
+//       school_admin_id: school_admin_id
+//     }
+//   });
+//   if (!classRecord) {
+//     throw new AppError('No class record found or you do not have permission for this record', 404);
+//   }
+// }
 
-  res.status(200).json({
-    status: 'success',
-    data: classToFetch,
-  });
+// Update a class
+exports.updateClass = catchAsync(async (req, res, next) => {
+  await isBelongsToAdmin(req.params.id, req.school_admin_id);
+  factory.updateOne(Class, 'class_id')(req, res, next);
 });
-
-
 // Delete the class
 exports.deleteClass = catchAsync(async (req, res, next) => {
-  const classToDelete = await Class.findByPk(req.params.id);
-  
-  if (!classToDelete) {
-    return next(new AppError('No class found with that ID', 404));
-  }
-
-  await classToDelete.update({ active: false });
-
-  res.status(200).json({
-    status: 'success',
-    message: 'Class deleted successfully',
-  });
+  await isBelongsToAdmin(req.params.id, req.school_admin_id);
+  factory.deleteOne(Class, 'class_id')(req, res, next);
 });
