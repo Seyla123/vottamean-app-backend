05f529b82fc34b063201c03bc8b99b3106ab0eca
Sveita <sveitast7@gmail.com>
2024-09-17 13:56:16 +0700

    refactor : refactor subject crud api

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -8,50 +8,78 @@ const AppError = require('../utils/appError');
 // Factory handler
 const factory = require('./handlerFactory');
 
-// Create a new subject
-exports.createSubject = catchAsync(async (req, res, next) => {
-  const { name, description } = req.body;
+const { filterObj } = require('../utils/filterObj');
 
-  const newSubject = await Subject.create({
-    name,
-    description,
-  });
-
-  if (!newSubject) {
-    return next(new AppError('Failed to create subject', 500));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      subject: newSubject,
+const checkIfBelongs = async (id, school_admin_id) => {
+  // Validate the ID
+  const subject = await Subject.findOne({
+    where: {
+      subject_id: id, // Assuming 'id' is the primary key of the Period model
+      school_admin_id: school_admin_id,
     },
   });
+  if (!subject) {
+    throw new AppError(
+      'No subject record found or you do not have permission for this record',
+      404
+    );
+  }
+};
+
+// check if any duplicate to prevent create the same period
+const checkDuplicate = async (name , school_admin_id) => {
+  const subject = await Subject.findOne({
+    where: {
+      name: name,
+      school_admin_id : school_admin_id, 
+      active : true
+    },
+  });
+  if (subject) {
+    throw new AppError('Subject already exists', 400);
+  }
+};
+
+// Create a new subject
+exports.createSubject = catchAsync(async (req, res, next) => {
+  const school_admin_id = req.school_admin_id; 
+  const { name } = req.body;
+
+  // filter the request body
+  req.body = filterObj(req.body, 'name' , 'description');
+
+  req.body.school_admin_id = school_admin_id;
+
+  // If no subject with the same name exists, create a new one
+  await checkDuplicate(name, school_admin_id);
+
+  // create new subject
+  await factory.createOne(Subject)(req, res, next);
+
 });
 
 // Get all subjects
-exports.getAllSubjects = factory.getAll(Subject);
+exports.getAllSubjects = catchAsync(async (req, res, next) => {
+  factory.getAll(Subject, { school_admin_id: req.school_admin_id })(req, res, next);
+});
 
 // Get a subject by ID
-exports.getSubjectById = factory.getOne(Subject, 'subject_id');
+exports.getSubjectById = catchAsync(async (req, res, next) => {
+  const subjectId = req.params.id;
+  await checkIfBelongs(subjectId, req.school_admin_id);
+  factory.getOne(Subject, 'subject_id')(req, res, next);
+});
 
 // Update a subject by ID
-exports.updateSubject = factory.updateOne(Subject, 'subject_id');
+exports.updateSubject = catchAsync(async (req, res, next) => {
+  const subjectId = req.params.id;
+  await checkIfBelongs(subjectId, req.school_admin_id);
+  factory.updateOne(Subject, 'subject_id')(req, res, next);
+});
 
 // Delete a subject by ID
 exports.deleteSubject = catchAsync(async (req, res, next) => {
   const subjectId = req.params.id;
-
-  const deletedSubject = await Subject.destroy({
-    where: { subject_id: subjectId },
-  });
-
-  if (!deletedSubject) {
-    return next(new AppError('No subject found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    message: 'Subject has been deleted',
-  });
+  await checkIfBelongs(subjectId, req.school_admin_id);
+  factory.deleteOne(Subject, 'subject_id')(req, res, next);
 });
