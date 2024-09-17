664768b137ffbac07fb71d170754ce2c11910516
Seav <Mrrseyla.758@gmail.com>
2024-09-17 14:43:51 +0700

    fix : utils helper

--- utils/helper.js
+++ utils/helper.js
@@ -1,11 +1,13 @@
 const AppError = require('./appError');
 //Checking for belongs to admin id
-exports.isBelongsToAdmin = async (id, field,school_admin_id,Model) => {
+exports.isBelongsToAdmin = async (id, field,school_admin_id,Model,field2) => {
     // Validate the ID
+
+    const additional = field2?{[field2]:school_admin_id} : {school_admin_id}
     const isExists = await Model.findOne({
       where: {
         [field]: id, // Assuming 'id' is the primary key of the model
-        school_admin_id,
+        ...additional
       },
     });
     if (!isExists) {
@@ -14,4 +16,6 @@ exports.isBelongsToAdmin = async (id, field,school_admin_id,Model) => {
         404
       );
     }
+    console.log(isExists);
+    
 };
