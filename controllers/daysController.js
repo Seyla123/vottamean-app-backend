b1bfe5015e8b7baddff18632c0d3dc8495ab1ee0
Sveita <sveitast7@gmail.com>
2024-09-17 10:49:44 +0700

    feature : fetch days

--- controllers/daysController.js
+++ controllers/daysController.js
@@ -1,5 +1,19 @@
-const Day = require('../models/dayModel');
+const {DayOfWeek} = require('../models/dayModel');
 
-const factory = require('./handlerFactory');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getAllDays = factory.getAll(Day);
+exports.getAllDays = catchAsync(async(req, res, next) => {
+    const response = await DayOfWeek.findAll()
+    
+    if(!response) {
+        return next (new AppError('Fail to load days', 404))
+
+    }
+
+    return res.status(200).json({
+        status: 'success',
+        data: response
+    })
+})
+
