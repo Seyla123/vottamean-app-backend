111cde7865355c9424dd0090d0e52d149dec255f
Seang Makaramarady <seangdy085@gmail.com>
2024-10-07 17:52:09 +0700

    resolve : solve conflict

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -195,6 +195,7 @@ exports.signupTeacher = catchAsync(async (req, res, next) => {
   // const verificationUrl = `${req.protocol}://${req.get(
   //   'host'
   // )}/api/v1/teachers/verify-email/teacher/${verificationToken}?token=${tempToken}`;
+  // const verificationUrl = `http://localhost:8000/auth/verify-teacher-email/${verificationToken}?token=${tempToken}`;
   const verificationUrl = `${req.headers.origin}/auth/verify-teacher-email/${verificationToken}?token=${tempToken}`;
 
   try {
