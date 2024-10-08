a341dcad68ad02afebb3b59dfafd13a657e0e1c9
Seang Makaramarady <seangdy085@gmail.com>
2024-10-08 17:46:05 +0700

    feature : implement routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,16 +1,27 @@
 // Import necessary modules
 const express = require('express');
-const bodyParser = require('body-parser');
 
 // Import Controllers
 const authController = require('../controllers/authController');
 const paymentController = require('../controllers/paymentController');
 
-const { rawBodyMiddleware } = require('../middlewares/rawBodyMiddleware');
-
 // Define Express Router
 const router = express.Router();
 
+// Stripe webhook endpoint (for handling payment events from Stripe)
+router.post(
+  '/webhook',
+  express.raw({ type: 'application/json' }),
+  paymentController.handleStripeWebhook,
+  (req, res) => {
+    console.log('Received webhook:', req.body);
+    res.status(200).json({
+      received: true,
+      status: 'success',
+      message: 'Webhook received',
+    });
+  }
+);
 // Protect all routes after this middleware
 router.use(authController.protect);
 
@@ -32,35 +43,6 @@ router.post('/create-payment-intent', paymentController.createPaymentIntent);
 router.post(
   '/create-checkout-session',
   paymentController.createCheckoutSession
-);
-
-// Stripe webhook endpoint (for handling payment events from Stripe)
-router.post(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
-  paymentController.handleStripeWebhook,
-  (req, res) => {
-    console.log('Received webhook:', req.body);
-    res.status(200).json({
-      received: true,
-      status: 'success',
-      message: 'Webhook received',
-    });
-  }
-);
-
-router.get(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
-  paymentController.handleStripeWebhook,
-  (req, res) => {
-    console.log('Received webhook:', req.body);
-    res.status(200).json({
-      received: true,
-      status: 'success',
-      message: 'Webhook received',
-    });
-  }
 );
 
 module.exports = router;
