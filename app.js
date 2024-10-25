d7fe0d982597a613b9f0d4c8e430794552967403
Seav <Mrrseyla.758@gmail.com>
2024-10-25 11:00:34 +0700

    file : add cors domian guest page

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'https://vottamean.com',
+      'https://www.vottamean.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
